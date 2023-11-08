6ee8f09b54d0e3db48bcd857330eb3e8011f1643
Sins-code <sebi.inserthere@gmail.com>
2023-11-08 17:55:55 +0100

    setup and requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dssshomework2",
+    version="0.1",
+    author="SinsCode",
+    packages=find_packages(),
+    install_requires=["wheel"],
+)
