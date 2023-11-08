3a49bcb8de1f7df6690169a6204b17ca7f1fab5c
Sins-code <sebi.inserthere@gmail.com>
2023-11-08 15:42:19 +0100

    math quiz changed

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,47 +1,90 @@
 import random
 
+# TODO
+#  better readability: variables and functions
+#  comments: add comments to code
+#  docstrings: for every function
+#  error handling: at least one try/except on user input
+#  fix bugs: with unit tests
 
-def function_A(min, max):
+
+def get_random_integer(min, max):
+    """ Returns a random integer between a minimum and maximum value.
+    Args:
+        min (int): The minimum value of returned integer.
+        max (int): The maximum value of returned integer.
+
+    Returns:
+        int: random integer
     """
-    Random integer.
-    """
+    try:
+        assert type(min) == int
+    except AssertionError:
+        print("Wrong input type")
     return random.randint(min, max)
 
 
-def function_B():
+def select_random_operation():
+    """ Returns randomly either the mathematical operation '+', '-' or '*'.
+
+        Returns:
+            str: The randomly selected operation.
+        """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def compute_math_result(number1, number2, operation):
+    """ Computes the result of the mathematical operation between two numbers.
+        Args:
+            number1 (int): First number.
+            number2 (int): Second Number.
+            operation (str): Mathematical operation.
+
+        Returns:
+            int: Result of mathematical operation.
+        """
+    # Create problem statement
+    problem = f"{number1} {operation} {number2}"
+    # Depending on operation compute result and return it
+    if operation == '+':
+        answer = number1 + number2
+    elif operation == '-':
+        answer = number1 - number2
+    else:
+        answer = number1 * number2
+    return problem, answer
 
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """ Runs a mathematical quiz on the command line."""
+    score = 0
+    amount_questions = 10
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    # run quiz
+    for _ in range(amount_questions):
+        # get two random numbers and an operation
+        number_1 = get_random_integer(1, 10); number_2 = get_random_integer(1, 5); operation = select_random_operation()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
+        # get problem statement and solution
+        problem, correct_answer = compute_math_result(number_1, number_2, operation)
+        print(f"\nQuestion: {problem}")
+        # collect user answer
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
+        # compare answers
+        if useranswer == correct_answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {correct_answer}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    # print final score
+    print(f"\nGame over! Your score is: {score}/{amount_questions}")
+
 
 if __name__ == "__main__":
     math_quiz()
